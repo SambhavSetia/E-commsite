42a4c2c3b74bc78902f4de00a2f28c6e105c6f9d
SambhavSetia <sambhavsetia29@gmail.com>
2025-01-13 23:39:36 +0530

    responsive navbar

--- Frontend/src/Components/Navbar/Navbar.jsx
+++ Frontend/src/Components/Navbar/Navbar.jsx
@@ -1,43 +1,74 @@
-import React, { useContext, useState } from 'react'
-// import { BrowserRouter, Routes, Route } from "react-router-dom";
-
-import './Navbar.css'
-import logo from '../Assets/logo.png';
-import cart_icon from '../Assets/cart_icon.png';
-import { Link } from 'react-router-dom';
-import { ShopContext } from '../../Context/ShopContext';
+import React, { useContext, useRef, useState } from "react";
+import logo from "../Assets/logo.png";
+import cart_icon from "../Assets/cart_icon.png";
+import { Link } from "react-router-dom";
+import "./Navbar.css";
+import { ShopContext } from "../../Context/ShopContext";
+import nav_dropdown from '../Assets/nav_dropdown.png'
 const Navbar = () => {
-  const [menu,SetMenu]=useState("SHOP");
-  const{getTotalCartItems}=useContext(ShopContext)
-  return (
-    <div className='Navbar'>
-        <div className="nav-logo">
-            <img src={logo} alt=''/>
-            <p>SHOPPER</p>
-        </div>
-       <div className="tops">
-            <ul className='top'>
-              <li onClick={()=>{SetMenu("SHOP")}}>
-                <Link style={{textDecoration:'None'}} to='/'>SHOP</Link>
-                {menu=="SHOP"?<hr></hr>:<></>}
-                </li> 
-              
-              <li onClick={()=>{SetMenu("MEN")}}><Link style={{textDecoration:'None'}} to='/mens'>MEN</Link>{menu=="MEN"?<hr></hr>:<></>}</li> 
-              <li onClick={()=>{SetMenu("WOMEN")}}><Link style={{textDecoration:'None'}} to='/women'>WOMEN</Link>{menu=="WOMEN"?<hr></hr>:<></>}</li> 
-             <li onClick={()=>{SetMenu("Kids")}}><Link style={{textDecoration:'None'}} to='/kids'>Kids</Link>{menu=="Kids"?<hr></hr>:<></>}</li>  </ul>
-                </div>
-           
-        
-        <div className="BTN">
-           <Link to='/LoginSignup'><button>lOGIN</button></Link> 
-        </div>
-       <Link to='/Cart'><img src={cart_icon} alt="" /></Link> 
-        <div className="cart_no">
-            {getTotalCartItems()}
-        </div>
-    </div>
+  const [menu, setMenu] = useState("shop");
+  const {getTotalCartItems}=useContext(ShopContext);
+  const menuRef=useRef();
 
-  )
-}
+  const nav_dropdown_toggle=(e)=>{
+    menuRef.current.classList.toggle('nav-menu-visible');
+    e.target.classList.toggle('open');
+
+
+  }
+  return (
+    <div className="navbar">
+      <div className="nav-logo">
+        <img  src={logo}></img>
+        <p>SHOPPER</p>
+      </div>
+      <img className="nav-dropdown" onClick={nav_dropdown_toggle} src={nav_dropdown} alt="" />
+      <ul ref={menuRef} className="nav-menu">
+        <li
+          onClick={() => {
+            setMenu("shop");
+          }}
+        >
+          <Link to="/"style={{textDecoration:'none'}}>SHOP</Link>
+          {menu === "shop" ? <hr /> : <></>}
+        </li>
+        <li
+          onClick={() => {
+            setMenu("mens");
+          }}
+        >
+          <Link to="/mens" style={{textDecoration:'none'}}>MEN</Link>
+          {menu === "mens" ? <hr /> : <></>}
+        </li>
+        <li
+          onClick={() => {
+            setMenu("womens");
+          }}
+        >
+          <Link to="women" style={{textDecoration:'none'}}>WOMEN</Link>
+          {menu === "womens" ? <hr /> : <></>}
+        </li>
+        <li
+          onClick={() => {
+            setMenu("kids");
+          }}
+        >
+          <Link to="/kids" style={{textDecoration:'none'}}>KIDS</Link>
+          {menu === "kids" ? <hr /> : <></>}
+        </li>
+      </ul>
+      <div className="nav-login-cart">
+        <button>
+          <Link to="/loginsignup" style={{textDecoration:'none'}}>Login</Link>
+        </button>
+        <Link to="/cart" style={{textDecoration:'none'}}>
+          <img src={cart_icon} alt=""></img>
+        </Link>
+
+        <div className="nav-cart-count">{getTotalCartItems()}</div>
+      </div>
+    </div>
+  );
+};
 
 export default Navbar;
