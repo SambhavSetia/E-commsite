55f16f5f3d7432e8909180fa566e4d7797997aba
SambhavSetia <sambhavsetia29@gmail.com>
2025-01-15 16:05:11 +0530

    responsiveness

--- Frontend/src/Components/Hero/Hero.jsx
+++ Frontend/src/Components/Hero/Hero.jsx
@@ -1,30 +1,32 @@
-import React from 'react'
-import './Hero.css'
-import hand_icon from'../Assets/hand_icon.png'
-import hero_image from'../Assets/hero_image.png'
+import React from 'react';
+import './Hero.css';
+import hand_icon from '../Assets/hand_icon.png';
+import hero_image from '../Assets/hero_image.png';
 
 const Hero = () => {
   return (
-    <div className='hero'>
-        <div className="hero_left">
-           <h2>New Arrivals Only</h2> 
-           
-           <div className="hero_left_hand">
-            <p>New</p>
-            <img src={hand_icon} alt='hand'></img>
-           </div>
-           <p>Collections</p>
-           <p>For Everyone</p>
-           <button>Latest Collections</button>
-
-
-
+    <section className='hero'>
+      {/* Left Section */}
+      <div className="hero_left">
+        <h2>New Arrivals Only</h2>
+        
+        <div className="hero_left_hand">
+          <p>New</p>
+          <img src={hand_icon} alt='Hand Icon' />
         </div>
 
-        <div className="hero_right"></div>
-        <img src={hero_image} alt="" />
-    </div>
-  )
-}
+        <p>Collections</p>
+        <p>For Everyone</p>
+        
+        <button>Latest Collections</button>
+      </div>
 
-export default Hero
+      {/* Right Section */}
+      <div className="hero_right">
+        <img src={hero_image} alt="Hero Display" />
+      </div>
+    </section>
+  );
+};
+
+export default Hero;
